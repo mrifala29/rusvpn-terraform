4bbd50e24dd61cf578a927b861ce12181abc08fa
mrifala29 <mrifala29@gmail.com>
2026-07-29 16:22:21 +0700

    Update gitignore

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
+    }
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
